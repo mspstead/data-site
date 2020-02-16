fc81709c804007a4579143bee366ff1c12f2e93b
mspstead <mspstead@gmail.com>
2020-02-16 21:36:10 +0000

    added css scripts and updated routing and images.

--- personal-site/src/app/app-routing.module.ts
+++ personal-site/src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component'
 import { BlogListComponent } from './blog-list/blog-list.component'
+import { FitbitDataComponent } from './fitbit-data/fitbit-data.component'
 import { AppComponent } from './app.component'
 
 
 const routes: Routes = [
         { path: 'about', component: AboutComponent},
-        { path: '', component: BlogListComponent}
+        { path: '', component: BlogListComponent},
+        { path: 'blog', component: BlogListComponent},
+        { path: 'blog/fitbit', component: FitbitDataComponent},
       ];
 
 @NgModule({
@@ -15,4 +18,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [AboutComponent,BlogListComponent]
+export const routingComponents = [AboutComponent,BlogListComponent,FitbitDataComponent]
