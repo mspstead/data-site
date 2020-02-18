34f285897771e9705e0a3b16317d02f54d44e27d
mspstead <mspstead@gmail.com>
2020-02-18 21:49:21 +0000

    Removed test print statements from price performance aws lambda function

--- lambda functions/price_performance/lambda_function.py
+++ lambda functions/price_performance/lambda_function.py
@@ -13,18 +13,3 @@ def lambda_handler(event, context):
     results = ia.getPerformanceMetrics()
 
     return results
-
-
-event = {
-    "BasketComposition": [
-        {"Identifier": "VFEM.L","Shares":20},
-        {"Identifier": "VWRL.L","Shares":40},
-        {"Identifier": "VMID.L","Shares":20}
-    ],
-    "Currency": "GBP",
-    "StartDate": "2018-01-01",
-    "EndDate": "2020-02-14"
-
-}
-res = lambda_handler(event,'')
-print(res)
